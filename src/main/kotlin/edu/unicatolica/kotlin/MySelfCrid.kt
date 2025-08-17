4b23714229717b4ef323e75006aad89c6ccb8e3b
Crid <contact@crid.com>
2025-08-17 14:03:18 -0500

    Subiendo mySelf Crid

---
+++ src/main/kotlin/edu/unicatolica/kotlin/MySelfCrid.kt
@@ -0,0 +1,11 @@
+package edu.unicatolica.kotlin
+
+fun main (){
+    val name: String = "Crid Dahiana Renteria"
+    val age: Int = 23
+    val program: String = "Tec.Desarrollo de software"
+    val interest: String = "fortalecer mis conocimientos"
+
+    val message = "Hola mi nombre es $name, tengo $age años y estoy en el programa $program, mi interes es $interest"
+    println(message)
+}
